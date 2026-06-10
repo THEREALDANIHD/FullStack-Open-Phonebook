a12390c94448a4aa21601358440bc00e835658ff
THEREALDANIHD <katajarahkolauri@gmail.com>
2026-06-10 15:02:29 +0300

    3.22

--- index.js
+++ index.js
@@ -11,101 +11,101 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 morgan.token('body', request => {
-    return JSON.stringify(request.body)
+  return JSON.stringify(request.body)
 })
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({}).then(person => {
-        response.json(person)
-    })
+  Person.find({}).then(person => {
+    response.json(person)
+  })
     .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    Person.find({}).countDocuments({})
-        .then(count => {
-            response.send(
-                `<div>
+  Person.find({}).countDocuments({})
+    .then(count => {
+      response.send(
+        `<div>
                     <p>Phonebook has info for ${count} people</p>
                     <p>${Date()}</p>
                 </div>`)
-        } )
+    } )
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            if(person) {
-                response.json(person)
-            }
-            else {
-                response.status(404).end()
-            }
+  Person.findById(request.params.id)
+    .then(person => {
+      if(person) {
+        response.json(person)
+      }
+      else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
-
-app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
-
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number } = request.body
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-    Person.findById(request.params.id)
-        .then(person => {
-            if(!person) {
-                return response.status(404).end()
-            }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-            person.name = name
-            person.number = number
-
-            return person.save().then((updatedPerson) => {
-                response.json(updatedPerson)
-            })
-        })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+    .catch(error => next(error))
 })
 
-const unknownEndpoint = (request, response, next) => {
-    response.status(404).send({error: "unknown endpoint"})
+app.put('/api/persons/:id', (request, response) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if(!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error)
+  console.log(error)
 
-    if(error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    if(error.name === 'ValidationError') {
-        return response.status(400).send({ error: error.message })
-    }
+  if(error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if(error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+  console.log(`server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -7,38 +7,38 @@ const url = process.env.MONGODB_URI
 console.log('connecting to ', url)
 
 mongoose.connect(url, { family: 4 })
-        .then(result => {
-            console.log('connected to MongoDB')
-        })
-        .catch((error) => {
-            console.log('error connectin to MongoDB', error.message)
-        })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connectin to MongoDB', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    number: {
-        type: String,
-        minlength: 8,
-        required: true,
-        validate: {
-            validator: function(value) {
-                return /^\d{2,3}-\d{7,8}$/.test(value)
-            },
-            message: "invalid phonenumber"
-        }
-    },
-    name: {
-        type: String,
-        minlength: 3,
-        require: true
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: function(value) {
+        return /^\d{2,3}-\d{7,8}$/.test(value)
+      },
+      message: 'invalid phonenumber'
     }
+  },
+  name: {
+    type: String,
+    minlength: 3,
+    require: true
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnObject) => {
-        returnObject.id = returnObject._id.toString()
-        delete returnObject._id
-        delete returnObject.__v
-    }
+  transform: (document, returnObject) => {
+    returnObject.id = returnObject._id.toString()
+    delete returnObject._id
+    delete returnObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
